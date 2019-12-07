74175daa00cc8362a060b9299508dc1d35d8c7e0
Tomas V <t.vestartas@gmail.com>
2019-12-07 15:49:28 +0200

    4 uzd

---
+++ src/loopsandarrays/TriangleRectangleChristmasTree.java
@@ -0,0 +1,32 @@
+package loopsandarrays;
+
+public class TriangleRectangleChristmasTree {
+    public static void main(String[] args) {
+
+        //RECTANGLE
+        //row
+        for (int i = 0; i < 5; i++) {
+            for (int a = 0; a < 5; a++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //Triangle
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("*");
+            System.out.println();
+
+
+
+
+            /*for (int a = 0; a < 5; a++) {
+                System.out.print("*");
+            }
+            System.out.println();*/
+        }
+
+    }
+}
+
