1c3655cf77b2ef4ad2fbd1992164b3b689ecc7d5
Tomas V <t.vestartas@gmail.com>
2019-12-07 12:23:02 +0200

    first program

--- src/basics/DoubleWithTwoDecimalPlaces.java
+++ src/basics/DoubleWithTwoDecimalPlaces.java
@@ -1,4 +1,14 @@
 package basics;
 
+import java.util.Scanner;
+
 public class DoubleWithTwoDecimalPlaces {
+    public static void main(String[] args) {
+        System.out.println("Iveskite skaiciu");
+        Scanner input = new Scanner(System.in);
+
+        double x = input.nextDouble();
+        System.out.println("Ivestas skaicius " + x + " suapvalintas atrodo taip:" + "\n");
+        System.out.printf("%.2f", x);
+    }
 }
